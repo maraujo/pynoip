92301da7406af7545535012ffc2f966619fbc408
Matheus Araujo <0matheus.araujo0@gmail.com>
2017-07-12 02:07:40 -0300

    adding port

--- requester.sh
+++ requester.sh
@@ -1,2 +1,2 @@
 #Use this in a cron tab
-curl -i -H "Content-Type: application/json" -X POST -d '{"label":"casa"}' 'http://noip.smellium.com/'
+curl -i -H "Content-Type: application/json" -X POST -d '{"label":"casa -p 2222"}' 'http://noip.smellium.com/'
